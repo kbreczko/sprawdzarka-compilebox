f29634364b3afe98ae23d64862023246ab7fcebf
kbreczko <kamil.breczko@gmail.com>
2018-11-30 20:45:44 +0100

    update programming language (#2)

--- API/compilers.js
+++ API/compilers.js
@@ -1,4 +1,3 @@
-
 /*
 	This file stores the compiler/interpretor details that are provided to DockerSandbox.sh	by the app.js
 	The index is the key field, 
@@ -16,19 +15,9 @@
 	*Revised on: 30th June 2014 (Added Column number 4 to display the name of languages to console)
 */
 
-exports.compilerArray= [ ["python","file.py","","Python",""],
-			 ["ruby","file.rb","","Ruby",""],
-			 ["clojure","file.clj","","Clojure",""],
-			 ["php","file.php","","Php",""],
-			 ["nodejs","file.js","","Nodejs",""],
-			 ["scala","file.scala","","Scala",""],
-			 ["\'go run\'","file.go","","Go",""],
-			 ["\'g++ -o /usercode/a.out\' ","file.cpp","/usercode/a.out","C/C++",""],
-			 ["javac","file.java","\'./usercode/javaRunner.sh\'","Java",""],
-			 ["\'vbnc -nologo -quiet\'","file.vb","\'mono /usercode/file.exe\'","VB.Net",""],
-			 ["gmcs","file.cs","\'mono /usercode/file.exe\'","C#",""],
-			 ["/bin/bash","file.sh"," ","Bash",""],
-			 ["gcc ","file.m"," /usercode/a.out","Objective-C","\' -o /usercode/a.out -I/usr/include/GNUstep -L/usr/lib/GNUstep -lobjc -lgnustep-base -Wall -fconstant-string-class=NSConstantString\'"],
-			 ["/usercode/sql_runner.sh","file.sql","","MYSQL",""],
-			 ["perl","file.pl","","Perl",""],
-			 ["\'env HOME=/opt/rust /opt/rust/.cargo/bin/rustc\'","file.rs","/usercode/a.out","Rust","\'-o /usercode/a.out\'"] ];
+exports.compilerArray = [["python", "file.py", "", "Python", ""],
+  ["nodejs", "file.js", "", "Nodejs", ""],
+  ["\'g++ -o /usercode/a.out\' ", "file.cpp", "/usercode/a.out", "C/C++", ""],
+  ["javac", "file.java", "\'./usercode/javaRunner.sh\'", "Java", ""],
+  ["/bin/bash", "file.sh", " ", "Bash", ""],
+  ["gcc ", "file.m", " /usercode/a.out", "Objective-C", "\' -o /usercode/a.out -I/usr/include/GNUstep -L/usr/lib/GNUstep -lobjc -lgnustep-base -Wall -fconstant-string-class=NSConstantString\'"]];
